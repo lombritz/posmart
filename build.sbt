eaf6a68d6d0f023f94bb790b87e4789a794d42be
lombritz <jaime@codefirstlab.com>
2016-06-18 10:42:02 -0400

    [NEW] Setup swing config.

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "posmart"
+
+version := "1.0.0"
+
+scalaVersion := "2.10.6"
+
+libraryDependencies ++= Seq(
+  "org.scala-lang" % "scala-swing" % "2.10.6"
+)
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,14 @@
+import scala.swing.{Frame, SimpleSwingApplication}
+
+/**
+  * Created by jaime on 6/17/16.
+  */
+object Main extends SimpleSwingApplication {
+
+  override def main(args: Array[String]) {
+
+  }
+
+  override def top: Frame = ???
+
+}
